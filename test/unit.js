9c6162999f7990ffd3667e88e88060fcb08c503f
Michael Ficarra <git@michael.ficarra.me>
2014-06-23 08:55:06 -0700

    use assertion helpers in unit tests

--- test/unit.js
+++ test/unit.js
@@ -3,7 +3,8 @@
 var assert = require("assert");
 
 var esvalid = require("..");
-var isValid = esvalid.isValid;
+function valid(x, msg) { assert.ok(esvalid.isValid(x), msg); }
+function notValid(x, msg) { assert.ok(!esvalid.isValid(x), msg); }
 
 var STMT = {type: "EmptyStatement"};
 var EXPR = {type: "Literal", value: 0};
@@ -12,61 +13,63 @@ var ID = {type: "Identifier", name: "a"};
 suite("unit", function(){
 
   test("non-nodes", function() {
-    assert.ok(!isValid(null));
-    assert.ok(!isValid(0));
-    assert.ok(!isValid({}));
-    assert.ok(!isValid("Program"));
-    assert.ok(!isValid({type: null}));
-    assert.ok(!isValid({type: false}));
-    assert.ok(!isValid({type: ""}));
-    assert.ok(!isValid({type: "Node"}));
+    notValid(null);
+    notValid(0);
+    notValid({});
+    notValid("Program");
+    notValid({type: null});
+    notValid({type: false});
+    notValid({type: ""});
+    notValid({type: "Node"});
   });
 
   test("Node", function() {
-    assert.ok(!isValid({type: "EmptyStatement", loc: {}}));
-    assert.ok(!isValid({type: "EmptyStatement", loc: {start: null, end: 0}}));
-    assert.ok(!isValid({type: "EmptyStatement", loc: {start: {}, end: {}}}));
-    assert.ok(!isValid({type: "EmptyStatement", loc: {start: {line: "a", column: "b"}, end: {line: "a", column: "b"}}}));
-    assert.ok(!isValid({type: "EmptyStatement", loc: {start: {line: 0, column: 0}, end: {line: 1, column: 0}}}));
-    assert.ok(!isValid({type: "EmptyStatement", loc: {start: {line: 1, column: 0}, end: {line: 0, column: 0}}}));
-    assert.ok(!isValid({type: "EmptyStatement", loc: {start: {line: 1, column: -1}, end: {line: 1, column: 0}}}));
-    assert.ok(!isValid({type: "EmptyStatement", loc: {start: {line: 1, column: 0}, end: {line: 1, column: -1}}}));
-    assert.ok(!isValid({type: "EmptyStatement", loc: {source: 0, start: {line: 1, column: 0}, end: {line: 1, column: 0}}}));
-    assert.ok(isValid({type: "EmptyStatement", loc: {start: {line: 1, column: 0}, end: {line: 1, column: 0}}}));
-    assert.ok(isValid({type: "EmptyStatement", loc: {source: "", start: {line: 1, column: 0}, end: {line: 1, column: 0}}}));
-    assert.ok(isValid({type: "EmptyStatement", loc: null}));
+    notValid({type: "EmptyStatement", loc: {}});
+    notValid({type: "EmptyStatement", loc: {start: null, end: 0}});
+    notValid({type: "EmptyStatement", loc: {start: {}, end: {}}});
+    notValid({type: "EmptyStatement", loc: {start: {line: "a", column: "b"}, end: {line: "a", column: "b"}}});
+    notValid({type: "EmptyStatement", loc: {start: {line: 0, column: 0}, end: {line: 1, column: 0}}});
+    notValid({type: "EmptyStatement", loc: {start: {line: 1, column: 0}, end: {line: 0, column: 0}}});
+    notValid({type: "EmptyStatement", loc: {start: {line: 1, column: -1}, end: {line: 1, column: 0}}});
+    notValid({type: "EmptyStatement", loc: {start: {line: 1, column: 0}, end: {line: 1, column: -1}}});
+    notValid({type: "EmptyStatement", loc: {source: 0, start: {line: 1, column: 0}, end: {line: 1, column: 0}}});
+    valid({type: "EmptyStatement", loc: {start: {line: 1, column: 0}, end: {line: 1, column: 0}}});
+    valid({type: "EmptyStatement", loc: {source: "", start: {line: 1, column: 0}, end: {line: 1, column: 0}}});
+    valid({type: "EmptyStatement", loc: null});
   });
 
 
   test("Program", function() {
-    assert.ok(!isValid({type: "Program"}));
-    assert.ok(!isValid({type: "Program", body: null}));
-    assert.ok(isValid({type: "Program", body: []}));
-    assert.ok(isValid({type: "Program", body: [STMT]}));
+    notValid({type: "Program"});
+    notValid({type: "Program", body: null});
+    valid({type: "Program", body: []});
+    valid({type: "Program", body: [STMT]});
+    valid({type: "Program", body: [STMT, STMT]});
+    notValid({type: "Program", body: [STMT, EXPR, STMT]});
   });
 
   test("SequenceExpression", function() {
-    assert.ok(!isValid({type: "SequenceExpression"}));
-    assert.ok(!isValid({type: "SequenceExpression", expressions: null}));
-    assert.ok(!isValid({type: "SequenceExpression", expressions: []}));
-    assert.ok(!isValid({type: "SequenceExpression", expressions: [EXPR]}));
-    assert.ok(isValid({type: "SequenceExpression", expressions: [EXPR, EXPR]}));
+    notValid({type: "SequenceExpression"});
+    notValid({type: "SequenceExpression", expressions: null});
+    notValid({type: "SequenceExpression", expressions: []});
+    notValid({type: "SequenceExpression", expressions: [EXPR]});
+    valid({type: "SequenceExpression", expressions: [EXPR, EXPR]});
   });
 
   test("SwitchStatement", function() {
-    assert.ok(!isValid({type: "SwitchStatement", discriminant: EXPR}));
-    assert.ok(!isValid({type: "SwitchStatement", discriminant: EXPR, cases: null}));
-    assert.ok(!isValid({type: "SwitchStatement", discriminant: EXPR, cases: []}));
-    assert.ok(isValid({type: "SwitchStatement", discriminant: EXPR, cases: [{type: "SwitchCase", test: EXPR, consequent: []}]}));
+    notValid({type: "SwitchStatement", discriminant: EXPR});
+    notValid({type: "SwitchStatement", discriminant: EXPR, cases: null});
+    notValid({type: "SwitchStatement", discriminant: EXPR, cases: []});
+    valid({type: "SwitchStatement", discriminant: EXPR, cases: [{type: "SwitchCase", test: EXPR, consequent: []}]});
   });
 
   test("VariableDeclaration", function() {
-    assert.ok(!isValid({type: "VariableDeclaration"}));
-    assert.ok(!isValid({type: "VariableDeclaration", declarations: null}));
-    assert.ok(!isValid({type: "VariableDeclaration", declarations: []}));
-    assert.ok(isValid({type: "VariableDeclaration", declarations: [{type: "VariableDeclarator", id: ID}]}));
-    assert.ok(isValid({type: "VariableDeclaration", declarations: [{type: "VariableDeclarator", id: ID, init: EXPR}]}));
-    assert.ok(isValid({type: "VariableDeclaration", declarations: [{type: "VariableDeclarator", id: ID}, {type: "VariableDeclarator", id: ID}]}));
+    notValid({type: "VariableDeclaration"});
+    notValid({type: "VariableDeclaration", declarations: null});
+    notValid({type: "VariableDeclaration", declarations: []});
+    valid({type: "VariableDeclaration", declarations: [{type: "VariableDeclarator", id: ID}]});
+    valid({type: "VariableDeclaration", declarations: [{type: "VariableDeclarator", id: ID, init: EXPR}]});
+    valid({type: "VariableDeclaration", declarations: [{type: "VariableDeclarator", id: ID}, {type: "VariableDeclarator", id: ID}]});
   });
 
 });
