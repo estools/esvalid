08750bd1efe2611d0948880ec0d25c4f604841c6
Michael Ficarra <git@michael.ficarra.me>
2014-07-26 22:27:03 -0700

    add everything.js to test suite

--- test/libraries.js
+++ test/libraries.js
@@ -6,57 +6,34 @@ var fs = require("fs");
 var esprima = require("esprima");
 var esvalid = require("..");
 
-function validate(done) {
-  return function(err, programText) {
-    if (err) throw err;
-    var program = esprima.parse(programText);
-    assert.ok(esvalid.isValid(program));
-    done();
+function validate(lib) {
+  return function test(done) {
+    fs.readFile(require.resolve(lib), function(err, programText) {
+      if (err) throw err;
+      var program = esprima.parse("" + programText);
+      assert.ok(esvalid.isValid(program));
+      done();
+    });
   };
 }
 
-if (!process.env.REALCOVERAGE)
-// top 10 most popular npm packages
-suite("popular libraries", function(){
+if (!process.env.REALCOVERAGE) {
 
-  test("underscore", function(done) {
-    fs.readFile(require.resolve("underscore"), validate(done));
+  suite("everything.js", function() {
+    test("is valid", validate("everything.js"));
   });
 
-  test("async", function(done) {
-    fs.readFile(require.resolve("async"), validate(done));
+  suite("top 10 most popular npm packages", function() {
+    test("underscore", validate("underscore"));
+    test("async", validate("async"));
+    test("request", validate("request"));
+    test("lodash", validate("lodash"));
+    test("commander", validate("commander"));
+    test("express", validate("express"));
+    test("optimist", validate("optimist"));
+    test("coffee-script", validate("coffee-script"));
+    test("colors", validate("colors"));
+    test("mkdirp", validate("mkdirp"));
   });
 
-  test("request", function(done) {
-    fs.readFile(require.resolve("request"), validate(done));
-  });
-
-  test("lodash", function(done) {
-    fs.readFile(require.resolve("lodash"), validate(done));
-  });
-
-  test("commander", function(done) {
-    fs.readFile(require.resolve("commander"), validate(done));
-  });
-
-  test("express", function(done) {
-    fs.readFile(require.resolve("express"), validate(done));
-  });
-
-  test("optimist", function(done) {
-    fs.readFile(require.resolve("optimist"), validate(done));
-  });
-
-  test("coffee-script", function(done) {
-    fs.readFile(require.resolve("coffee-script"), validate(done));
-  });
-
-  test("colors", function(done) {
-    fs.readFile(require.resolve("colors"), validate(done));
-  });
-
-  test("mkdirp", function(done) {
-    fs.readFile(require.resolve("mkdirp"), validate(done));
-  });
-
-});
+}
